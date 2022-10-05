280dde1b15bf09ba74abfb3c25bb4bb0dbee2974
Farid Hidayat <e.faridhidayat@gmail.com>
2022-10-06 00:34:12 +0700

    feat: ApiFilesResourcesController

---
+++ src/Controllers/ApiFilesResourcesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SaltFile\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use SaltLaravel\Models\Resources;
+use Spatie\Permission\Exceptions\UnauthorizedException;
+use SaltLaravel\Services\ResponseService;
+use SaltLaravel\Controllers\ApiResourcesController;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Model;
+
+class ApiFilesResourcesController extends ApiResourcesController
+{
+    protected $modelNamespace = 'SaltFile';
+}
