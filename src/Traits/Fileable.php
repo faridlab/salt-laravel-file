b78b52316be7673bcf09441711906ca6f603e50f
Farid Hidayat <e.faridhidayat@gmail.com>
2022-11-10 02:21:04 +0700

    refactor: handling multiple/array of files input upload by user

--- src/Traits/Fileable.php
+++ src/Traits/Fileable.php
@@ -56,34 +56,37 @@ trait Fileable
                 }
 
                 $file = request()->file($field);
-                $filename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
-                $filename = $filename.'-'.time().'.'.$file->getClientOriginalExtension();
-                $data['filename'] = $filename;
-                $data['ext'] = $file->getClientOriginalExtension();
-                $data['size'] = $file->getSize();
-                if(env('FILESYSTEM_DRIVER', 'local') == 'gcs') {
-                    $disk = Storage::disk('gcs');
-                    $path = $disk->put($model->getTable(), $file);
-                    $imgurl = ['https://storage.googleapis.com', env('GOOGLE_CLOUD_STORAGE_BUCKET'), $path];
-                    $data['fullpath'] = implode('/', $imgurl);
-                    $data['path'] = $path;
-                } else { // local
-                    $path = $file->storeAs("public/{$dir}", $filename);
-                    $data['fullpath'] = url(str_replace('public', 'storage', $path));
-                    $data['path'] = $path;
-                }
-                $data['directory'] = $dir;
-                $data['type'] = $fileableType;
-
-                request()->request->remove($field);
-                unset(request()[$field]);
-                unset($model[$field]);
-                if($selfFileable == true) {
-                    foreach ($data as $field => $value) {
-                        $model->setAttribute($field, $value);
+                $files = is_array($file)? $file: [$file];
+                foreach($files as $file) {
+                    $filename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                    $filename = $filename.'-'.time().'.'.$file->getClientOriginalExtension();
+                    $data['filename'] = $filename;
+                    $data['ext'] = $file->getClientOriginalExtension();
+                    $data['size'] = $file->getSize();
+                    if(env('FILESYSTEM_DRIVER', 'local') == 'gcs') {
+                        $disk = Storage::disk('gcs');
+                        $path = $disk->put($model->getTable(), $file);
+                        $imgurl = ['https://storage.googleapis.com', env('GOOGLE_CLOUD_STORAGE_BUCKET'), $path];
+                        $data['fullpath'] = implode('/', $imgurl);
+                        $data['path'] = $path;
+                    } else { // local
+                        $path = $file->storeAs("public/{$dir}", $filename);
+                        $data['fullpath'] = url(str_replace('public', 'storage', $path));
+                        $data['path'] = $path;
                     }
-                } else {
-                    $model->fileableFiles[] = $data;
+                    $data['directory'] = $dir;
+                    $data['type'] = $fileableType;
+
+                    request()->request->remove($field);
+                    unset(request()[$field]);
+                    unset($model[$field]);
+                    if($selfFileable == true) {
+                        foreach ($data as $field => $value) {
+                            $model->setAttribute($field, $value);
+                        }
+                    } else {
+                        $model->fileableFiles[] = $data;
+                    }
                 }
             }
         });
@@ -115,35 +118,39 @@ trait Fileable
                 }
 
                 $file = request()->file($field);
-                $filename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
-                $filename = $filename.'-'.time().'.'.$file->getClientOriginalExtension();
-                $data['filename'] = $filename;
-                $data['ext'] = $file->getClientOriginalExtension();
-                $data['size'] = $file->getSize();
-                if(env('FILESYSTEM_DRIVER', 'local') == 'gcs') {
-                    $disk = Storage::disk('gcs');
-                    $path = $disk->put($model->getTable(), $file);
-                    $imgurl = ['https://storage.googleapis.com', env('GOOGLE_CLOUD_STORAGE_BUCKET'), $path];
-                    $data['fullpath'] = implode('/', $imgurl);
-                    $data['path'] = $path;
-                } else { // local
-                    $path = $file->storeAs("public/{$dir}", $filename);
-                    $data['fullpath'] = url(str_replace('public', 'storage', $path));
-                    $data['path'] = $path;
-                }
-                $data['directory'] = $dir;
-                $data['type'] = $fileableType;
-
-                request()->request->remove($field);
-                unset(request()[$field]);
-                unset($model[$field]);
-                if($selfFileable == true) {
-                    foreach ($data as $field => $value) {
-                        $model->setAttribute($field, $value);
+                $files = is_array($file)? $file: [$file];
+                foreach($files as $file) {
+                    $filename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                    $filename = $filename.'-'.time().'.'.$file->getClientOriginalExtension();
+                    $data['filename'] = $filename;
+                    $data['ext'] = $file->getClientOriginalExtension();
+                    $data['size'] = $file->getSize();
+                    if(env('FILESYSTEM_DRIVER', 'local') == 'gcs') {
+                        $disk = Storage::disk('gcs');
+                        $path = $disk->put($model->getTable(), $file);
+                        $imgurl = ['https://storage.googleapis.com', env('GOOGLE_CLOUD_STORAGE_BUCKET'), $path];
+                        $data['fullpath'] = implode('/', $imgurl);
+                        $data['path'] = $path;
+                    } else { // local
+                        $path = $file->storeAs("public/{$dir}", $filename);
+                        $data['fullpath'] = url(str_replace('public', 'storage', $path));
+                        $data['path'] = $path;
                     }
-                } else {
-                    $model->fileableFiles[] = $data;
+                    $data['directory'] = $dir;
+                    $data['type'] = $fileableType;
+
+                    request()->request->remove($field);
+                    unset(request()[$field]);
+                    unset($model[$field]);
+                    if($selfFileable == true) {
+                        foreach ($data as $field => $value) {
+                            $model->setAttribute($field, $value);
+                        }
+                    } else {
+                        $model->fileableFiles[] = $data;
+                    }
                 }
+
             }
         });
 
